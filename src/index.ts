b44c876978ba7812a8a82b4e0698dd3205a598ae
duxter <dlyarabotiacc457@gmail.com>
2026-07-15 19:41:52 +0300

    imported image now is deleted

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ const db:{users:usersType[], videos:videosType[]} = {
         {id: 5, title:'VIDEO S USHASTIM', resolution: 720},
         {id: 6, title:'VIDEO S PINGVINOM', resolution: 1080}]};
 app.get('/', (req, res) => {
-    res.sendFile("C:\\Users\\Public\\BackEndDimych\\my-first-project\\images\\super.jpg")
+    res.send('САМЫЙ КРУТОЙ САЙТ НА СВЕТЕ!!!!!!!!!!!!!!!!!!')
 });
 
 app.get('/users/', (req: RequestWithQuery<GetUserQueryModel>, res: Response<UserViewModel[]>) => {
